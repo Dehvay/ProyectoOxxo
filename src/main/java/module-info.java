1c822627b68245ce274c4d6f0d80cf9248a1b3fd
Dehvay <dymg.ngeva@outlook.com>
2024-05-15 17:31:45 -0600

    INICIO DEL PROYECTO

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module fes.aragon.proyectooxxo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens fes.aragon.proyectooxxo to javafx.fxml;
+    exports fes.aragon.proyectooxxo;
+}
