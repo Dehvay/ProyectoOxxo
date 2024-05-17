b2d15db22649eafa058db8d928fd837d458fea63
F3rnand0Ur13l <ferchouriel.mi>
2024-05-17 15:24:13 -0600

    Maquetado Version 1

---
+++ src/main/java/fes/aragon/proyectooxxo/controller/ProveedoresController.java
@@ -0,0 +1,52 @@
+package fes.aragon.proyectooxxo.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+public class ProveedoresController {
+
+    @FXML
+    private TableColumn<?, ?> cmlCorreo;
+
+    @FXML
+    private TableColumn<?, ?> cmlDireccion;
+
+    @FXML
+    private TableColumn<?, ?> cmlImagen;
+
+    @FXML
+    private TableColumn<?, ?> cmlNombre;
+
+    @FXML
+    private TableColumn<?, ?> cmlOperaciones;
+
+    @FXML
+    private TableColumn<?, ?> cmlTelefono;
+
+    @FXML
+    private Button idAgregarProveedor;
+
+    @FXML
+    private Button idArchivoProveedor;
+
+    @FXML
+    private Button idGuardarProveedor;
+
+    @FXML
+    void eventoAgregarProveedor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eventoArchivoProveedor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eventoGuardarProveedor(ActionEvent event) {
+
+    }
+
+}
